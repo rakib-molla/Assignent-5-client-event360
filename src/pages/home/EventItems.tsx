98fb59d61cd7bb96259fa4290b214713083262f7
rakib-molla <rakibmail1997@gmail.com>
2024-01-31 22:03:40 +0600

    event item

--- src/pages/home/EventItems.tsx
+++ src/pages/home/EventItems.tsx
@@ -8,8 +8,8 @@ function EventItems() {
         <p className="max-w-[80ch] mt-10 mb-10 ">Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh ornare viverra. Ultrices faucibus neque velit risus ac id lorem.</p>
         </div>
 
-        <div className="grid grid-cols-12 ">
-            <div className="border col-span-12 md:col-span-8">
+        <div className="grid grid-cols-12 gap-3 ">
+            <div className=" col-span-12 md:col-span-8">
                 <div className="grid grid-cols-12 gap-5">
                     <div className="col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
                             <img className="h-[150px] cursor-pointer contrast-50 w-[100%] hover:contrast-100 transition-all object-cover rounded-md" src="https://img.freepik.com/free-photo/view-3d-person-sleeping-clouds_23-2151113525.jpg?t=st=1706700661~exp=1706704261~hmac=dc1084ff02ac85a0d03049def5384119301485246236bc581f011755f2b872fd&w=740" alt="" />
@@ -21,26 +21,25 @@ function EventItems() {
                         <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 4</h1>
 
                     </div>
+
                     <div className=" col-span-12 md:col-span-6 ">
                         <div className="flex items-center justify-center gap-2">
-                            <div className="border  col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
+                            <div className="  col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
                             <img className="h-[150px] cursor-pointer contrast-50 w-[100%] hover:contrast-100 transition-all object-cover rounded-md " src="https://img.freepik.com/free-photo/view-3d-person-sleeping-clouds_23-2151113525.jpg?t=st=1706700661~exp=1706704261~hmac=dc1084ff02ac85a0d03049def5384119301485246236bc581f011755f2b872fd&w=740" alt="" />
-                        <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 2</h1>
+                            <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 2</h1>
+                        </div>
 
-                    </div>
-
-                    <div className=" col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
-                            <img className="h-[150px] cursor-pointer contrast-50 w-[100%] hover:contrast-100 transition-all object-cover rounded-md " src="https://img.freepik.com/free-photo/view-3d-person-sleeping-clouds_23-2151113525.jpg?t=st=1706700661~exp=1706704261~hmac=dc1084ff02ac85a0d03049def5384119301485246236bc581f011755f2b872fd&w=740" alt="" />
-                        <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 3</h1>
-
-                    </div>
-
+                            <div className=" col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
+                                <img className="h-[150px] cursor-pointer contrast-50 w-[100%] hover:contrast-100 transition-all object-cover rounded-md " src="https://img.freepik.com/free-photo/view-3d-person-sleeping-clouds_23-2151113525.jpg?t=st=1706700661~exp=1706704261~hmac=dc1084ff02ac85a0d03049def5384119301485246236bc581f011755f2b872fd&w=740" alt="" />
+                                <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 3</h1>
+                            </div>
                         </div>
                     </div>
+
+
                     <div className=" col-span-12 md:col-span-6  bg-[#F0F0F5] p-2 rounded-md">
                             <img className="h-[150px] cursor-pointer contrast-50 w-[100%] hover:contrast-100 transition-all object-cover rounded-md" src="https://img.freepik.com/free-photo/view-3d-person-sleeping-clouds_23-2151113525.jpg?t=st=1706700661~exp=1706704261~hmac=dc1084ff02ac85a0d03049def5384119301485246236bc581f011755f2b872fd&w=740" alt="" />
                         <h1 className="font-bold ps-6 flex py-2 items-center">Event Item - 5</h1>
-
                     </div>
                 </div>
                 
@@ -53,6 +52,7 @@ function EventItems() {
             </div>
             
         </div>
+     
 
     </Container>
   )

bfb9504792b477f0f152330740008815e9b7b90c
rakib-molla <rakibmail1997@gmail.com>
2024-01-31 12:06:42 +0600

    footer create

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,60 +1,61 @@
 import { ArrowRight, EuroIcon, PlusIcon, WholeWord } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <section>
-      <footer className="bg-gray-500 p-5">
-        <div className="grid lg:grid-cols-6 md:grid-cols-3 justify-center gap-10 justify-items-center">
-          <div className="flex flex-col">
-            <h1 className="text-3xl font-bold text-white">Event <span className="text-[#3461FF]">360</span></h1>
+      <footer className=" bg-gradient-to-r from-[#353836] to-[#302F37]">
+        <div className="grid lg:grid-cols-6 md:grid-cols-3 justify-center gap-10 md:justify-items-center">
+          <div className="">
+            <h1 className="text-3xl font-bold text-white text-left me-16">Event <span className="text-[#3461FF]">360</span></h1>
           </div>
-          <div>
+          <div className="text-white">
             <h1 className="font-bold">Product</h1>
             <p>Pricing</p>
             <p>Overview</p>
             <p>Browse</p>
             <p>Accessibility</p>
           </div>
-          <div>
+          <div className="text-white">
             <h1 className="font-bold">Solutions</h1>
             <p>Brainstorming</p>
             <p>ideation</p>
             <p>wireframing</p>
             <p>Research</p>
           </div>
-          <div>
+          <div className="text-white">
             <h1 className="font-bold">Resources</h1>
             <p>Help Center</p>
             <p>Blog</p>
             <p>Tutorials</p>
             <p>FAQs</p>
           </div>
-          <div>
+          <div className="text-white">
             <h1 className="font-bold">Support</h1>
             <p>Contact Us</p>
             <p>Developers</p>
             <p>Documentation</p>
             <p>Integrations</p>
           </div>
-          <div>
+          <div className="text-white">
             <h1 className="font-bold">Company</h1>
             <p>About</p>
             <p>Press</p>
             <p>Events</p>
-            <p className="flex items-start gap-2">
+            <Link to={'/'} className="flex items-start gap-2">
               Request Demo{" "}
               <span>
                 <ArrowRight />
               </span>
-            </p>
+            </Link>
           </div>
         </div>
         <div>
-          <h1 className="border border-red-500 my-5"></h1>
+          <h1 className="border border-dashed border-gray-300 my-5 px-10 w-[95%] mx-auto"></h1>
         </div>
-        <div className="flex items-start justify-between gap-5 border flex-wrap">
-          <div>@2023.All Right Reserved</div>
-          <div className="flex gap-5 items-center flex-wrap">
+        <div className="flex items-start justify-between gap-5 flex-wrap px-10 p-10">
+          <div className="text-white">@2023.All Right Reserved</div>
+          <div className="flex gap-5 items-center flex-wrap text-white">
             <span>Terms</span>
             <span>Privacy</span>
             <span>Contact</span>

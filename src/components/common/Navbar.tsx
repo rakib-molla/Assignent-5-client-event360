17fe77c56b35cf5669e2c50e2fd2b54403d1293a
rakib-molla <rakibmail1997@gmail.com>
2024-02-02 19:11:45 +0600

    hero section add card

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,6 +2,7 @@ import { NavLink } from 'react-router-dom'
 import { motion } from 'framer-motion';
 import heroSectionImage from '@/assets/img/heroSectionImage.png'
 import Container from '@/layouts/Container';
+import { Heart } from 'lucide-react';
 
 
 function Navbar() {
@@ -26,16 +27,34 @@ function Navbar() {
     </motion.header>
 
    {/* hero section*/}
-    <div className='flex border max-w-[1230px] px-[20px] mx-auto md:flex-nowrap  flex-wrap  gap-10'>
+    <div className='flex border max-w-[1230px] px-[20px] mx-auto md:flex-nowrap  flex-wrap  gap-5'>
 <div>
-    <h1 className='text-6xl font-bold'>Brand New <br /> Event Packages <span className='custom-font-color'>For Winter</span></h1>
-    <p className='max-w-[50ch] my-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. Faucibus nec gravida ipsum pulvinar vel non.</p>
+    <h1 className='text-6xl font-bold mt-10'>Brand New <br /> Event Packages <br /> <span className='custom-font-color'>For Winter</span></h1>
+    <p className='max-w-[40ch] my-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. Faucibus nec gravida ipsum pulvinar vel non.</p>
     <button className='bg-[#3461FF] text-white py-2 px-10'>Explore</button>
-</div>
+
+
+    <div className='flex gap-5 mt-5  w-full'>
+        <div className='border-2 border-white p-3 rounded-3xl bg-[#EFEDF9] shadow-sm'>
+            <Heart className=' size-12  fill-[#685FD4] text-[#C3BFED] bg-[#C3BFED] p-2 rounded-full '/>
+            <p className='max-w-[20ch] mt-8 text-[#333333] font-thin'>Lorem ipsum dolor sit amet, consectetu adipiscing elit. Cursus imperdiet sed id.</p>
+        </div>
+        <div className='border-2 border-white p-3 rounded-3xl bg-[#EFEDF9] shadow-sm'>
+            <Heart className=' size-12  fill-[#F2C94C] text-[#F2E3BD] bg-[#F2E3BD] p-2 rounded-full '/>
+            <p className='max-w-[20ch] mt-8 text-[#333333] font-thin'>Lorem ipsum dolor sit amet, consectetu adipiscing elit. Cursus imperdiet sed id.</p>
+        </div>
+        <div className='border-2 border-white p-3 rounded-3xl bg-[#EFEDF9] shadow-sm'>
+            <Heart className=' size-12  fill-[#8AB2DC] text-[#CDDBF0] bg-[#CDDBF0] p-2 rounded-full '/>
+            <p className='max-w-[20ch] mt-8 text-[#333333] font-thin'>Lorem ipsum dolor sit amet, consectetu adipiscing elit. Cursus imperdiet sed id.</p>
+        </div>
+    </div>
+
+    </div>
     
-    <div className='flex w-full mx-auto items-center justify-center'>
+    <div className='flex  mx-auto items-center justify-center'>
         <img src={heroSectionImage} alt="" />
     </div>
+
 
 
     </div>
